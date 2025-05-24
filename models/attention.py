6d66f68dd223025f7e2d4007913af702a82888a2
Swapnajoy Saha <swapnajoy.saha@rwth-aachen.de>
2025-05-24 20:46:35 +0200

    added the module for Multi-Head Attention

---
+++ models/attention.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+class MultiHeadAttentionBlock(nn.Module):
+    def __init__(self, embed_dim, hidden_dim, num_heads):
+        super().__init__()
+        self.num_heads = num_heads
+        self.head_dim = hidden_dim // num_heads
+        self.Wq = nn.Linear(embed_dim, hidden_dim)
+        self.Wk = nn.Linear(embed_dim, hidden_dim)
+        self.Wv = nn.Linear(embed_dim, hidden_dim)
+        self.Wo = nn.Linear(hidden_dim, embed_dim)
+
+    def forward(self,x):
+        Q = self.Wq(x)
+        K = self.Wk(x)
+        V = self.Wv(x)
+
+        Q = Q.reshape(Q.shape[0], Q.shape[1], self.num_heads, self.head_dim)
+        K = K.reshape(K.shape[0], K.shape[1], self.num_heads, self.head_dim)
+        V = V.reshape(V.shape[0], V.shape[1], self.num_heads, self.head_dim)
+
+        Q = Q.permute(0, 2, 1, 3)
+        K = K.permute(0, 2, 1, 3)
+        V = V.permute(0, 2, 1, 3)
+
+        attention_weights = F.softmax(torch.matmul(Q, K.transpose(-2,-1))/math.sqrt(self.head_dim), dim = -1)
+        weighted_values = torch.matmul(attention_weights,V)
+
+        weighted_values = weighted_values.permute(0, 2, 1, 3)
+        weighted_values = weighted_values.reshape(weighted_values.shape[0], weighted_values.shape[1], -1)
+
+        return self.Wo(weighted_values)
