4ab1c9e6eedee685e0c0dce78934be145179fc5d
Swapnajoy Saha <swapnajoy.saha@rwth-aachen.de>
2025-05-25 17:59:33 +0200

    updated to move one level up in hierarchy and added to sys path

--- models/rnn_LSTM.py
+++ models/rnn_LSTM.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import torch
 import torch.nn as nn
 from models.embedding import EmbeddingLayer
