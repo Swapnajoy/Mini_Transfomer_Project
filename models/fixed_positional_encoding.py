099d7bcf15ea47c8b5007bc1f6e95eb6bc5bee8a
Swapnajoy Saha <swapnajoy.saha@rwth-aachen.de>
2025-05-25 12:00:16 +0200

    modified the pos embeddings to use vectorized encoding generation and removed the testing statements

--- models/fixed_positional_encoding.py
+++ models/fixed_positional_encoding.py
@@ -2,24 +2,19 @@ import torch
 import torch.nn as nn
 import math
 
-print("hi")
 class SinusoidalPositionalEncoding(nn.Module):
     def __init__(self, max_seq_len, embed_dim):
         super().__init__()
         pe = torch.empty((max_seq_len, embed_dim), dtype=torch.float32)
-        for i in range(max_seq_len):
-            for j in range(embed_dim):
-                if j%2 == 0:
-                    pe[i, j] = math.sin(i/math.pow(10000, j/embed_dim))
-                else:
-                    pe[i, j] = math.cos(i/math.pow(10000, (j-1)/embed_dim))
+
+        pos = torch.arange(0, max_seq_len).unsqueeze(1).float()
+        div_term = torch.exp(torch.arange(0, embed_dim, 2).float() * (-math.log(10000.0) / embed_dim))
+
+        pe [:, 0::2] = torch.sin(pos * div_term)
+        pe [:, 1::2] = torch.cos(pos * div_term)
         
         self.register_buffer("sinusoidal_positional_encoding", pe)
 
     def forward(self, x):
         seq_len = x.shape[1]
         return x + self.sinusoidal_positional_encoding[:seq_len,:]
-
-model = SinusoidalPositionalEncoding(6, 10)
-x = torch.rand((128, 6, 10))
-print(model(x).shape)
