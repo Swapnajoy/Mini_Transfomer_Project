89896cfcbec833ab3a34dc7fc61361c96a2a20fa
Swapnajoy Saha <swapnajoy.saha@rwth-aachen.de>
2025-05-25 21:47:11 +0200

    updated to handle unknown words and padding, added function to get vocab_size

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -1,10 +1,10 @@
 import os
 
 class Tokenizer:
-    def __init__(self):
+    def __init__(self, file_path):
         words = set()
         
-        with open(r"data\alice_in_wonderland.txt", encoding="utf-8") as corpus:
+        with open(file_path, encoding="utf-8") as corpus:
             for line in corpus:
                 words.update(line.strip().split())
 
@@ -12,8 +12,14 @@ class Tokenizer:
         self.stoi = {word:i for i, word in enumerate(self.vocab)}
         self.itos = {i: word for word, i in self.stoi.items()}
 
+        self.vocab.insert(0, '<unk>')       #to handle unseen words during inference
+        self.vocab.insert(0, '<pad>')       #to handle batching with padding
+
     def encode(self, text: str) -> list[int]:
         return [self.stoi[word] for word in text.strip().split()]
     
     def decode(self, indices: list[int]) -> str:
         return " ".join([self.itos[i] for i in indices])
+    
+    def vocab_size(self):
+        return len(self.vocab)
