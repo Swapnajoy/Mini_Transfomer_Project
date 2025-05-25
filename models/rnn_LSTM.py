d7fa716ea05ee32fca5a48aba35de527e8466f56
Swapnajoy Saha <swapnajoy.saha@rwth-aachen.de>
2025-05-25 17:28:58 +0200

    added an LSTM module for comparison later

---
+++ models/rnn_LSTM.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+from models.embedding import EmbeddingLayer
+
+class LSTM(nn.Module):
+    def __init__(self, seq_len, vocab_size, embed_dim, hidden_dim):
+        super().__init__()
+        self.embed = EmbeddingLayer(vocab_size, embed_dim)
+        self.LSTM = nn.LSTM(embed_dim, hidden_dim, batch_first=True)
+        self.dp = nn.Dropout(p=0.2)
+        self.lin = nn.Linear(hidden_dim, vocab_size)
+
+    def forward(self, x):
+        x = self.embed(x)
+        out, _ = self.LSTM(x)
+        return self.lin(self.dp(out))
