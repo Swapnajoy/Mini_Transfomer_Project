0875945433d02d22dbf2b4663996853414798f28
Swapnajoy Saha <swapnajoy.saha@rwth-aachen.de>
2025-05-26 21:36:02 +0200

    removed inplace updates

--- models/transformer_block.py
+++ models/transformer_block.py
@@ -18,6 +18,6 @@ class EncoderBlock(nn.Module):
         self.dp2 = nn.Dropout(p=0.2)
 
     def forward(self, x):
-        x += self.dp1(self.attention(self.ln1(x)))
-        x += self.dp2(self.feedforward(self.ln2(x)))
+        x = x + self.dp1(self.attention(self.ln1(x)))
+        x = x + self.dp2(self.feedforward(self.ln2(x)))
         return x
