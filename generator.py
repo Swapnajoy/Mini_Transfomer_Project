432f68924a81d579d2dd96c7aee137e2f805933b
Swapnajoy <swapnajoy.saha@rwth-aachen.de>
2025-05-27 13:33:07 +0000

    generator script with greddy selection

---
+++ generator.py
@@ -0,0 +1,58 @@
+import os
+
+import torch
+import torch.nn as nn
+
+from utils.tokenizer import Tokenizer
+from models.transformerLM import TransformerLanguageModel
+
+checkpoint_path = "training_experiments/transformerLM_ep30_b32_lr0.0003_dataset_alice_in_wonderland.txt/model_epoch_30.pth"
+txt_file_path = "data/alice_in_wonderland.txt"
+
+tokenizer = Tokenizer(txt_file_path)
+vocab_size = tokenizer.vocab_size()
+
+seq_len = 8
+embed_dim = 128
+num_heads = 4
+hidden_dim = 128
+enc_ffn_h_dim = 512
+num_enc = 6
+use_sinusoidal = True
+
+device = ('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = TransformerLanguageModel(vocab_size, embed_dim, seq_len, hidden_dim, num_heads, enc_ffn_h_dim, num_enc, use_sinusoidal).to(device)
+
+model.load_state_dict(torch.load(checkpoint_path))
+
+model.eval()
+
+seed_text = "In a land far far away"
+
+generated_tokens = tokenizer.encode(seed_text)
+
+if len(generated_tokens) > seq_len:
+    input_tokens = torch.tensor(generated_tokens[-seq_len:]).unsqueeze(0).to(device)
+else:    
+    input_tokens = torch.tensor(generated_tokens).unsqueeze(0).to(device)
+
+#Generate 100 words
+
+with torch.no_grad():
+    for _ in range(100):
+        pred_logits = model(input_tokens)
+
+        next_token = torch.argmax(pred_logits[0, -1, :])
+
+        generated_tokens.append(next_token.cpu().item())
+
+        next_token = torch.tensor(next_token.to(device)).unsqueeze(0).unsqueeze(0)
+
+        input_tokens = torch.cat((input_tokens, next_token), dim = 1)
+
+        if input_tokens.shape[1] > seq_len:
+            input_tokens = input_tokens[:, -seq_len:]
+    
+    generated_words = tokenizer.decode(generated_tokens)
+    print(generated_words)
