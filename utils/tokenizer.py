4658cc5d27b0cc18ff8eb8498919ab4d273e95a9
Swapnajoy Saha <swapnajoy.saha@rwth-aachen.de>
2025-05-28 21:43:03 +0200

    char_level_tokenizer added

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -1,20 +1,20 @@
 class Tokenizer:
     def __init__(self, file_path):
-        words = set()
+        chars = set()
         
         with open(file_path, encoding="utf-8") as corpus:
             for line in corpus:
-                words.update(line.strip().split())
+                chars.update(ch for ch in line)
 
-        self.vocab = ['unk', 'pad'] + sorted(words)     #to handle unseen words during inference and batching with padding
-        self.stoi = {word:i for i, word in enumerate(self.vocab)}
-        self.itos = {i: word for word, i in self.stoi.items()}
+        self.vocab = sorted(chars)
+        self.stoi = {ch:i for i, ch in enumerate(self.vocab)}
+        self.itos = {i: ch for ch, i in self.stoi.items()}
 
     def encode(self, text: str) -> list[int]:
-        return [self.stoi.get(word, self.stoi['unk']) for word in text.strip().split()]
+        return [self.stoi[ch] for ch in text]
     
     def decode(self, indices: list[int]) -> str:
-        return " ".join([self.itos[i] for i in indices])
+        return ''.join([self.itos[i] for i in indices])
     
     def vocab_size(self):
         return len(self.vocab)
