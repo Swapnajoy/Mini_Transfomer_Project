e7db1640359ee0c54a7d98b39fd08873a9cea2a9
Swapnajoy <swapnajoy.saha@rwth-aachen.de>
2025-05-29 18:40:45 +0000

    updated text_dataset.py to directly take tokens

--- datasets/text_dataset.py
+++ datasets/text_dataset.py
@@ -1,15 +1,11 @@
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import torch
 from torch.utils.data import Dataset
 from utils.tokenizer import Tokenizer
 
 class TextDataset(Dataset):
-    def __init__(self, file_path, seq_len):
-        self.tokenizer = Tokenizer(file_path)
-        self.tokens = self.tokenizer.encode(open(file_path, encoding='utf-8').read())
+    def __init__(self, tokens, seq_len):
+        
+        self.tokens = tokens
         self.seq_len = seq_len
 
     def __len__(self):
@@ -19,7 +15,3 @@ class TextDataset(Dataset):
         x = torch.tensor(self.tokens[index:index+self.seq_len], dtype=torch.long)
         y = torch.tensor(self.tokens[index+1:index+1+self.seq_len], dtype=torch.long)
         return x, y
-    
-    @property
-    def vocab_size(self):
-        return len(self.tokenizer.vocab)
