068916cc5840904f265364994e304902f33d0b80
Swapnajoy <swapnajoy.saha@rwth-aachen.de>
2025-05-29 18:35:46 +0000

    modified tokenizer to handle either file_path or text

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -1,20 +1,23 @@
 class Tokenizer:
-    def __init__(self, file_path):
-        chars = set()
+    def __init__(self, file_path=None, text=None):
+        #requires either file_path or text
+        assert (text is not None) or (file_path is not None)
         
-        with open(file_path, encoding="utf-8") as corpus:
-            for line in corpus:
-                chars.update(ch for ch in line)
+        if text is None:
+            with open(file_path, encoding="utf-8") as corpus:
+                text = corpus.read()
 
-        self.vocab = sorted(chars)
-        self.stoi = {ch:i for i, ch in enumerate(self.vocab)}
+        chars = sorted(set(text))
+        self.vocab = chars
+        self.stoi = {ch: i for i, ch in enumerate(chars)}
         self.itos = {i: ch for ch, i in self.stoi.items()}
 
     def encode(self, text: str) -> list[int]:
         return [self.stoi[ch] for ch in text]
-    
+
     def decode(self, indices: list[int]) -> str:
         return ''.join([self.itos[i] for i in indices])
     
+    @property
     def vocab_size(self):
         return len(self.vocab)
