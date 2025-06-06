40ab9f3301cb9fce9f11091c02027849858af06a
Swapnajoy <swapnajoy.saha@rwth-aachen.de>
2025-06-06 09:18:41 +0000

    Corrected the PAD and BOS tokens

--- datasets/seq2seq_dataset.py
+++ datasets/seq2seq_dataset.py
@@ -10,7 +10,7 @@ def encode_pair(src, tgt, tokenizer, max_seq_len=128):
 
     src_ids = src_encoded.ids[:max_seq_len]
     tgt_ids = tgt_encoded.ids[:max_seq_len]
-
+    
     return src_ids, tgt_ids
 
 class Seq2SeqDataset(Dataset):
@@ -19,21 +19,22 @@ class Seq2SeqDataset(Dataset):
         self.tgt_list = tgt_list
         self.tokenizer = tokenizer
         self.max_seq_len = max_seq_len
-        self.pad_id = tokenizer.token_to_id('<pad>')
+        self.pad_id = tokenizer.token_to_id('[PAD]')
 
     def __len__(self):
         return len(self.src_list)
     
     def __getitem__(self, idx):
         src_ids, tgt_ids = encode_pair(self.src_list[idx], self.tgt_list[idx], self.tokenizer, self.max_seq_len)
+
         src_ids += [self.pad_id] * (self.max_seq_len - len(src_ids))
         tgt_ids += [self.pad_id] * (self.max_seq_len - len(tgt_ids))
 
-        dec_in = [self.tokenizer.token_to_id('<s>')] + tgt_ids[:-1]
+        dec_in = [self.tokenizer.token_to_id('[BOS]')] + tgt_ids[:-1]
         dec_tgt = tgt_ids
 
         return{
-            'src': torch.tensor(src_ids),
-            'tgt': torch.tensor(dec_in),
-            'label': torch.tensor(dec_tgt)
+            'src': torch.tensor(src_ids, dtype=torch.long),
+            'tgt': torch.tensor(dec_in, dtype=torch.long),
+            'label': torch.tensor(dec_tgt, dtype=torch.long)
         }
