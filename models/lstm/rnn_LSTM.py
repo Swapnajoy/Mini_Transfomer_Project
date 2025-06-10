97b5c085b2f45e8971fbf9ef0766055a7b4ce277
Swapnajoy Saha <swapnajoy.saha@rwth-aachen.de>
2025-06-10 18:42:51 +0200

    Removed sys path append

--- models/lstm/rnn_LSTM.py
+++ models/lstm/rnn_LSTM.py
@@ -1,8 +1,3 @@
-import os
-import sys
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import torch
 import torch.nn as nn
 from models.encoder_only.embedding import EmbeddingLayer
