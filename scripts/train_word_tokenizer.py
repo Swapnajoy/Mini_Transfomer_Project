11fb07209798b32dd61296ba71b8d0d1e60c7b58
Swapnajoy <swapnajoy.saha@rwth-aachen.de>
2025-06-12 07:49:27 +0000

    added training script for word tokenizer

---
+++ scripts/train_word_tokenizer.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.word_tokeinizer import WordTokenizer
+
+txt_file_path1 = "data/alice_in_wonderland.txt"
+txt_file_path2 = "data/tiny_shakespeare.txt"
+
+tokenizer = WordTokenizer(txt_file_path1)
+tokenizer.save("tokenizers/alice_word_tokenizer.json")
+print("Saving alice_word_tokenizer.json")
+
+tokenizer = WordTokenizer(txt_file_path2)
+tokenizer.save("tokenizers/shakespeare_word_tokenizer.json")
+print("Saving shakespeare_word_tokenizer.json")
